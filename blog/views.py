59a9a108d135acbc7986dc7d3b4347318d470aa2
tjlandry <tjj.landryy@gmail.com>
2019-11-05 20:01:30 -0600

    heroku

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
